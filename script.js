0dd327ae7085437a5809e99408031b71e1dd1c47
GustiSchonfeld <54253264+GustiSchonfeld@users.noreply.github.com>
2026-07-20 23:10:46 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Cambiar el fondo del menú al hacer scroll para mejorar legibilidad
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
